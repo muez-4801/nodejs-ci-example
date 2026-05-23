ce8024e201dfd905783633de880a9b31c21fb5b1
Muez Fathi <muez_4801@limu.edu.ly>
2026-05-23 14:16:39 +0200

    Add GitHub Actions workflow

---
+++ calculator.test.js
@@ -0,0 +1,13 @@
+const { add, subtract, multiply, divide } = require("./calculator");
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(add(1, 2)).toBe(3);
+});
+
+test("subtracts 5 - 3 to equal 2", () => {
+  expect(subtract(5, 3)).toBe(2);
+});
+
+test("throws error when dividing by zero", () => {
+  expect(() => divide(10, 0)).toThrow("Cannot divide by zero");
+});
