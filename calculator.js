e663301bb330c1829aa340c5aae113230095f800
Muez Fathi <muez_4801@limu.edu.ly>
2026-05-23 14:34:26 +0200

    Force CI failure for testing

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-function add(a, b) { return a + b; }
+function add(a, b) { return a + b + 10; }
 function subtract(a, b) { return a - b; }
 function multiply(a, b) { return a * b; }
 function divide(a, b) {
